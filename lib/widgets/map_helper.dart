42855ac927f4dbbee4212e052c05fd7d5c411af0
ivan kopech <ivankopech@gmail.com>
2025-03-20 15:13:49 -0300

    polyline entre puntos

--- lib/widgets/map_helper.dart
+++ lib/widgets/map_helper.dart
@@ -1,10 +1,7 @@
-import 'package:dio/dio.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class MapHelper {
   static Future<Set<Marker>> generateMarkers({
@@ -47,14 +44,24 @@ class MapHelper {
     LatLng originLatLng = await getLatLngFromAddress(origin);
     LatLng destinationLatLng = await getLatLngFromAddress(destination);
 
-    List<LatLng> routePoints =
-        await getRouteCoordinates(originLatLng, destinationLatLng, apiKey);
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      apiKey,
+      PointLatLng(originLatLng.latitude, originLatLng.longitude),
+      PointLatLng(destinationLatLng.latitude, destinationLatLng.longitude),
+    );
+
+    List<LatLng> polylineCoordinates = result.points
+        .map((PointLatLng point) => LatLng(point.latitude, point.longitude))
+        .toList();
 
     return Polyline(
       polylineId: const PolylineId('route'),
       color: Colors.indigo,
       width: 5,
-      points: [originLatLng, destinationLatLng],
+      points: polylineCoordinates.isNotEmpty
+          ? polylineCoordinates
+          : [originLatLng, destinationLatLng],
     );
   }
 
@@ -68,58 +75,5 @@ class MapHelper {
       debugPrint('error getting coordinates for address: $e');
     }
     return const LatLng(-32.944242, -60.650538);
-  }
-
-  static Future<List<LatLng>> getRouteCoordinates(
-      LatLng origin, LatLng destination, String apiKey) async {
-    final String url =
-        'https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${destination.latitude},${destination.longitude}&mode=driving&key=$apiKey';
-
-    try {
-      final response = await http.get(Uri.parse(url));
-
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        if ((data['routes'] as List).isNotEmpty) {
-          String encodedPolyline =
-              data['routes'][0]['overview_polyline']['points'];
-          return decodePolyline(encodedPolyline);
-        }
-      }
-    } catch (e) {
-      print('error fetching route: $e');
-    }
-    return [];
-  }
-
-  static List<LatLng> decodePolyline(String encoded) {
-    List<LatLng> polylineCoordinates = [];
-    int index = 0, len = encoded.length;
-    int lat = 0, lng = 0;
-
-    while (index < len) {
-      int shift = 0, result = 0;
-      int byte;
-      do {
-        byte = encoded.codeUnitAt(index++) - 63;
-        result |= (byte & 0x1F) << shift;
-        shift += 5;
-      } while (byte >= 0x20);
-      int deltaLat = ((result & 1) == 1 ? ~(result >> 1) : (result >> 1));
-      lat += deltaLat;
-
-      shift = 0;
-      result = 0;
-      do {
-        byte = encoded.codeUnitAt(index++) - 63;
-        result |= (byte & 0x1F) << shift;
-        shift += 5;
-      } while (byte >= 0x20);
-      int deltaLng = ((result & 1) == 1 ? ~(result >> 1) : (result >> 1));
-      lng += deltaLng;
-
-      polylineCoordinates.add(LatLng(lat / 1E5, lng / 1E5));
-    }
-    return polylineCoordinates;
   }
 }
