a95c9476966e105fbe4acf0d26b722af2b85e9ea
ivan kopech <ivankopech@gmail.com>
2025-04-08 13:22:17 -0300

    ruteo de arrival time

--- lib/config/router/router_provider.dart
+++ lib/config/router/router_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:move/config/observers/logging_route_observer.dart';
 import 'package:move/features/map/presentation/screens/screens.dart';
+import '../../features/map/presentation/screens/arrival_time.dart';
 
 final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
@@ -17,6 +18,11 @@ final goRouterProvider = Provider<GoRouter>((ref) {
         path: MapInputScreen.path,
         name: MapInputScreen.name,
         builder: (context, state) => const MapInputScreen(),
+      ),
+      GoRoute(
+        path: ArrivalTimeScreen.path,
+        name: ArrivalTimeScreen.name,
+        builder: (context, state) => const ArrivalTimeScreen(),
       ),
     ],
   );
