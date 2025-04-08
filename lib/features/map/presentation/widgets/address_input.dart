739a73b27bb1e6f36d34b0ae6e9e6cf2f0fe033c
ivan kopech <ivankopech@gmail.com>
2025-04-08 13:11:35 -0300

    parametros y sugerencia de direcciones

--- lib/features/map/presentation/widgets/address_input.dart
+++ lib/features/map/presentation/widgets/address_input.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_google_places_sdk/flutter_google_places_sdk.dart';
 import 'package:go_router/go_router.dart';
 
 import './custom_textfield.dart';
 
 class AddressInput extends StatefulWidget {
-  final TextEditingController originAddressController;
-  final TextEditingController destinationAddressController;
+  final bool isOrigin;
+  final TextEditingController controller;
+  final Function(String) onSelected;
+  final String? otherAddress;
 
   const AddressInput({
     super.key,
-    required this.originAddressController,
-    required this.destinationAddressController,
+    required this.isOrigin,
+    required this.controller,
+    required this.onSelected,
+    this.otherAddress,
   });
 
   @override
@@ -27,16 +31,20 @@ class _AddressInputState extends State<AddressInput> {
   List<String> destinationSuggestions = [];
   bool isOriginFocused = false;
   String apiKey = dotenv.env['API_KEY'] ?? '';
-  bool isTypingOrigin = false;
+  bool isTypingOrigin = true;
   late TextEditingController originController;
   late TextEditingController destinationController;
+  late FlutterGooglePlacesSdk places;
 
   @override
   void initState() {
     super.initState();
     loadEnv();
+
     originFocusNode = FocusNode();
     destinationFocusNode = FocusNode();
+
+    places = FlutterGooglePlacesSdk(apiKey);
 
     originFocusNode.addListener(() {
       setState(() {
@@ -67,6 +75,7 @@ class _AddressInputState extends State<AddressInput> {
     await dotenv.load();
     setState(() {
       apiKey = dotenv.env['API_KEY'] ?? '';
+      places = FlutterGooglePlacesSdk(apiKey);
     });
   }
 
@@ -88,60 +97,67 @@ class _AddressInputState extends State<AddressInput> {
       });
       return;
     }
-    const String baseUrl =
-        "https://maps.googleapis.com/maps/api/place/autocomplete/json";
-    final Uri uri = Uri.parse('$baseUrl?input=$input&key=$apiKey');
 
-    try {
-      final response = await Dio().get(uri.toString());
+    final result = await places.findAutocompletePredictions(input);
+    final suggestions =
+        result.predictions.map((p) {
+          final primaryText = p.primaryText;
+          final secondaryText = p.secondaryText;
+          return '$primaryText, $secondaryText';
+        }).toList();
 
-      if (response.statusCode == 200) {
-        final data = response.data;
-        setState(() {
-          if (isOrigin) {
-            originSuggestions =
-                (data['predictions'] as List)
-                    .map((item) => item['description'] as String)
-                    .toList();
-          } else {
-            destinationSuggestions =
-                (data['predictions'] as List)
-                    .map((item) => item['description'] as String)
-                    .toList();
-          }
-        });
-      }
-    } catch (e) {
-      print('error fetching suggestions: $e');
-    }
-  }
-
-  void onSuggestionTap(String address, bool isOrigin) {
     setState(() {
       if (isOrigin) {
-        widget.originAddressController.text = address;
-        originSuggestions.clear();
-        widget.originAddressController.selection = TextSelection.fromPosition(
-          const TextPosition(offset: 0),
-        );
+        originSuggestions = suggestions;
       } else {
-        widget.destinationAddressController.text = address;
+        destinationSuggestions = suggestions;
+      }
+    });
+  }
+
+  Future<void> onSuggestionTap(String suggestion, bool isOrigin) async {
+    final predictions = await places.findAutocompletePredictions(suggestion);
+    if (predictions.predictions.isEmpty) return;
+
+    final prediction = predictions.predictions.first;
+    final details = await places.fetchPlace(
+      prediction.placeId,
+      fields: [
+        PlaceField.AddressComponents,
+        PlaceField.Name,
+        PlaceField.Address,
+      ],
+    );
+
+    final address = details.place?.address ?? prediction.primaryText;
+
+    setState(() {
+      if (isOrigin) {
+        originController.text = address;
+        originSuggestions.clear();
+      } else {
+        destinationController.text = address;
         destinationSuggestions.clear();
-        widget.destinationAddressController.selection =
-            TextSelection.fromPosition(const TextPosition(offset: 0));
       }
     });
 
-    if (!isOrigin) {
-      // Navigator.pop(context, {
-      //   'origin': widget.originAddressController.text,
-      //   'destination': widget.destinationAddressController.text,
-      // });
+    if (originController.text.isNotEmpty &&
+        destinationController.text.isNotEmpty) {
       context.pop({
         'origin': originController.text,
         'destination': destinationController.text,
       });
     }
+  }
+
+  void handleTap(bool isOrigin) {
+    Future.microtask(() {
+      if (mounted) {
+        setState(() {
+          isTypingOrigin = isOrigin;
+        });
+      }
+    });
   }
 
   @override
@@ -154,7 +170,7 @@ class _AddressInputState extends State<AddressInput> {
           child: Column(
             children: [
               CustomTextField(
-                controller: originController,
+                controller: widget.controller,
                 focusNode: originFocusNode,
                 icon: const Icon(Icons.arrow_upward_outlined),
                 hintText: 'Enter origin location...',
@@ -162,15 +178,7 @@ class _AddressInputState extends State<AddressInput> {
                 suggestions: originSuggestions,
                 onSuggestionTap: onSuggestionTap,
                 fetchSuggestions: fetchSuggestions,
-                onTap: (isOrigin) {
-                  Future.microtask(() {
-                    if (mounted) {
-                      setState(() {
-                        isTypingOrigin = isOrigin;
-                      });
-                    }
-                  });
-                },
+                onTap: handleTap,
               ),
               const SizedBox(height: 15),
               CustomTextField(
