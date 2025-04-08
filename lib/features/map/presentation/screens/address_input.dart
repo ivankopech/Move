890184ab4ca7dfe279b09723399a714ea71af8b2
ivan kopech <ivankopech@gmail.com>
2025-04-08 13:10:26 -0300

    parametros nuevos

--- lib/features/map/presentation/screens/address_input.dart
+++ lib/features/map/presentation/screens/address_input.dart
@@ -4,7 +4,7 @@ import '../widgets/address_input.dart';
 
 class AddressInputScreen extends StatefulWidget {
   static const path = '/address-input';
-    static const name = 'address-input';
+  static const name = 'address-input';
 
   const AddressInputScreen({super.key});
 
@@ -30,11 +30,28 @@ class _AddressInputScreenState extends State<AddressInputScreen> {
     super.dispose();
   }
 
+  void handleSelected(String value, bool isOrigin) {
+    if (isOrigin) {
+      originAddressController?.text = value;
+    } else {
+      destinationAddressController?.text = value;
+    }
+
+    final origin = originAddressController?.text;
+    final destination = destinationAddressController?.text;
+
+    if (origin!.isNotEmpty && destination!.isNotEmpty) {
+      Navigator.of(context).pop({'origin': origin, 'destination': destination});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return AddressInput(
-      originAddressController: originAddressController!,
-      destinationAddressController: destinationAddressController!,
+      isOrigin: true,
+      controller: originAddressController!,
+      onSelected: (value) => handleSelected(value, true),
+      otherAddress: destinationAddressController?.text,
     );
   }
 }
