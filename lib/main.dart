ad76e1da8a1bf01236773346d6e00a31ff856b4c
ivan kopech <ivankopech@gmail.com>
2025-04-08 13:04:29 -0300

    saco debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,10 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final goRouter = ref.watch(goRouterProvider);
-    return MaterialApp.router(title: 'Flutter Demo', routerConfig: goRouter);
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      routerConfig: goRouter,
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
