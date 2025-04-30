57bd372d4a4d73f906c71e8ac7a7870a47f84adb
ivan kopech <ivankopech@gmail.com>
2025-04-30 16:26:07 -0300

    dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,19 @@ allprojects {
     }
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        add("classpath", "com.google.gms:google-services:4.4.2")
+
+        add("classpath", "com.android.tools.build:gradle:7.0.4")
+        add("classpath", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+    }
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
