7a2e42e172e4c3ce1cd2ca114167e800c412fe6c
ivan kopech <ivankopech@gmail.com>
2025-05-14 12:25:03 -0300

    square connection and create request

--- lib/features/map/presentation/widgets/payment_input_widget.dart
+++ lib/features/map/presentation/widgets/payment_input_widget.dart
@@ -1,81 +1,153 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_credit_card/flutter_credit_card.dart';
-// import 'package:square_in_app_payments/google_pay_constants.dart';
-// import 'package:square_in_app_payments/in_app_payments.dart';
-// import 'package:square_in_app_payments/models.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-import './custom_card_widget.dart';
+import 'package:move/features/map/data/models/create_request.dart';
+import 'package:move/features/map/presentation/providers/providers.dart';
+import 'dart:convert';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/create_request_state_notifier_provider.dart';
 
-class PaymentWidget extends StatefulWidget {
-  const PaymentWidget({super.key});
+class SquarePaymentWidget extends ConsumerStatefulWidget {
+  const SquarePaymentWidget({super.key});
 
   @override
-  State<PaymentWidget> createState() => _PaymentWidgetState();
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      SquarePaymentWidgetState();
 }
 
-class _PaymentWidgetState extends State<PaymentWidget> {
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+class SquarePaymentWidgetState extends ConsumerState<SquarePaymentWidget> {
+  late final WebViewController controller;
+  String token = '';
 
-  CreditCardModel? currentModel;
+  @override
+  void initState() {
+    super.initState();
+
+    initializeWebPayment();
+  }
+
+  void initializeWebPayment() async {
+    controller =
+        WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..addJavaScriptChannel(
+            'SquareChannel',
+            onMessageReceived: (message) async {
+              final data = jsonDecode(message.message);
+              token = data['token'];
+              print('el token es $token');
+              await sendRequest();
+              //llamar al endpoint y enviar token al backend
+            },
+          )
+          ..loadRequest(Uri.parse('https://move.softdev.ar/public/index.html'));
+  }
+
+  Future<void> sendRequest() async {
+    final origin = ref.watch(originAddressProvider);
+    if (origin == null) {
+      // Mostramos un error o retornamos
+      print('Origin is null');
+      return;
+    }
+    final destination = ref.watch(destinationAddressProvider);
+    final distance = ref.watch(distanceProvider);
+    final details = ref.watch(detailsProvider);
+    final images = ref.watch(imagesProvider);
+
+    final payment = Payment(paymentMethodId: 1, token: token);
+
+    List<Imagenes> imagesList = [
+      Imagenes(
+        nombre: images.nombre,
+        mimeType: images.mimeType,
+        data: images.data,
+      ),
+    ];
+
+    List<Tramos> tramos = [
+      Tramos(
+        nroTramo: 0,
+        existingTo: 0,
+        aliasTo: '',
+        streetTo: origin.street,
+        cpTo: origin.postalCode,
+        cityTo: origin.city,
+        numberTo: origin.number,
+        countryTo: origin.country,
+        florNummberTo: '',
+        stateTo: origin.state,
+        descriptionTo: details.description,
+        placeIdTo: '',
+        latTo: origin.latitude,
+        longTo: origin.longitude,
+        longDirectionTo: '',
+        nroRemito: '',
+        tamanioCarga: 1,
+        pesoCarga: 1,
+        acotacionesCarga: '',
+        imagenes: imagesList,
+      ),
+      Tramos(
+        nroTramo: 1,
+        existingTo: 0,
+        aliasTo: '',
+        streetTo: destination!.street,
+        cpTo: destination.postalCode,
+        cityTo: destination.city,
+        numberTo: destination.number,
+        countryTo: destination.country,
+        florNummberTo: '',
+        stateTo: destination.state,
+        descriptionTo: details.description,
+        placeIdTo: '',
+        latTo: destination.latitude,
+        longTo: destination.longitude,
+        longDirectionTo: '',
+        nroRemito: '',
+        tamanioCarga: 1,
+        pesoCarga: 1,
+        acotacionesCarga: '',
+        imagenes: imagesList,
+      ),
+    ];
+    await ref
+        .read(createRequestStateNotifierProvider.notifier)
+        .createRequest(
+          1,
+          '',
+          origin.street,
+          origin.postalCode,
+          origin.city,
+          origin.number,
+          origin.country,
+          '',
+          origin.state,
+          '',
+          '',
+          origin.latitude,
+          origin.longitude,
+          '',
+          details.description,
+          details.startDate,
+          details.startTime,
+          '',
+          2,
+          2,
+          tramos,
+          payment,
+          distance?.distance.toInt(),
+          0,
+          0,
+          0,
+        );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final requestsState = ref.watch(createRequestStateNotifierProvider);
     return Scaffold(
-      appBar: AppBar(title: Text('Insert payment information')),
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-            child: Column(
-              children: [
-                CustomCreditCardWidget(
-                  formKey: formKey,
-                  onModelChange: (model) => currentModel = model,
-                ),
-                const SizedBox(height: 100),
-                ElevatedButton(
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Valid card!')),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Wrong data')),
-                      );
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(padding: EdgeInsets.zero),
-
-                  child: Ink(
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        colors: [Colors.indigo, Colors.purple],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Container(
-                      constraints: const BoxConstraints(
-                        minWidth: 100,
-                        minHeight: 50,
-                      ),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "Continue",
-                        style: TextStyle(color: Colors.white, fontSize: 16),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      appBar: AppBar(title: Text('Insert a payment method')),
+      body: WebViewWidget(controller: controller),
     );
   }
 }
