cadbc33604bfe0088374d498bdd08b64b7461ce7
ivan kopech <ivankopech@gmail.com>
2025-05-14 12:21:46 -0300

    providers

--- lib/features/map/presentation/providers/providers.dart
+++ lib/features/map/presentation/providers/providers.dart
@@ -1,8 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:move/config/api_client_provider.dart';
 import 'package:move/features/map/data/repositories/repositories.dart';
 import 'package:move/features/map/domain/repositories/repositories.dart';
 import 'package:move/features/map/domain/use_cases/use_cases.dart';
+import 'package:move/features/map/presentation/providers/state/details_notifier.dart';
+import 'package:move/features/map/presentation/providers/state/images_notifier.dart';
+import '../../data/repositories/create_request_repository_interface_implementation.dart';
+import '../../domain/repositories/create_request_repository_interface.dart';
+import '../../domain/use_cases/create_request_use_case.dart';
+import '../../data/models/request_info.dart';
 
 // REPOSITORY PROVIDERS
 final sampleRepositoryInterfaceProvider = Provider<SampleRepositoryInterface>((
@@ -17,3 +24,35 @@ final sampleUseCaseProvider = Provider<SampleUseCase>(
     sampleRepositoryInterface: ref.watch(sampleRepositoryInterfaceProvider),
   ),
 );
+
+//CREATE REQUEST
+
+final createRequestRepositoryInterfaceProvider =
+    Provider<CreateRequestRepositoryInterface>((ref) {
+      final apiClient = ref.read(apiClientProvider);
+      return CreateRequestRepositoryInterfaceImplementation(apiClient);
+    });
+
+// USE CASE PROVIDERS
+final createRequestUseCaseProvider = Provider<CreateRequestUseCase>(
+  (ref) => CreateRequestUseCase(
+    createRequestRepositoryInterface: ref.watch(
+      createRequestRepositoryInterfaceProvider,
+    ),
+  ),
+);
+
+//PROVIDERS
+final originAddressProvider = StateProvider<AddressModel?>((ref) => null);
+final destinationAddressProvider = StateProvider<AddressModel?>((ref) => null);
+final distanceProvider = StateProvider<Distance?>((ref) => null);
+final imagesProvider = StateNotifierProvider<ImagesNotifier, ImagesModel>((
+  ref,
+) {
+  return ImagesNotifier();
+});
+final detailsProvider = StateNotifierProvider<DetailsNotifier, DetailsModel>((
+  ref,
+) {
+  return DetailsNotifier();
+});
