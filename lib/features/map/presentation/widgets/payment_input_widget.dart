3aa5bcd53bb15611b0945498d22bea282d422d45
ivan kopech <ivankopech@gmail.com>
2025-05-16 16:56:17 -0300

    returns success message and closes payment screen

--- lib/features/map/presentation/widgets/payment_input_widget.dart
+++ lib/features/map/presentation/widgets/payment_input_widget.dart
@@ -36,110 +36,139 @@ class SquarePaymentWidgetState extends ConsumerState<SquarePaymentWidget> {
               token = data['token'];
               print('el token es $token');
               await sendRequest();
-              //llamar al endpoint y enviar token al backend
             },
+          )
+          ..setNavigationDelegate(
+            NavigationDelegate(
+              onNavigationRequest: (request) {
+                if (request.url == 'success://home') {
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                  return NavigationDecision.prevent;
+                }
+                return NavigationDecision.navigate;
+              },
+            ),
           )
           ..loadRequest(Uri.parse('https://move.softdev.ar/public/index.html'));
   }
 
-  Future<void> sendRequest() async {
+  Future<bool> sendRequest() async {
     final origin = ref.watch(originAddressProvider);
     if (origin == null) {
       // Mostramos un error o retornamos
       print('Origin is null');
-      return;
+      return false;
     }
-    final destination = ref.watch(destinationAddressProvider);
-    final distance = ref.watch(distanceProvider);
-    final details = ref.watch(detailsProvider);
-    final images = ref.watch(imagesProvider);
+    try {
+      final destination = ref.watch(destinationAddressProvider);
+      final distance = ref.watch(distanceProvider);
+      final details = ref.watch(detailsProvider);
+      final images = ref.watch(imagesProvider);
 
-    final payment = Payment(paymentMethodId: 1, token: token);
+      final payment = Payment(paymentMethodId: 1, token: token);
 
-    List<Imagenes> imagesList = [
-      Imagenes(
-        nombre: images.nombre,
-        mimeType: images.mimeType,
-        data: images.data,
-      ),
-    ];
+      List<Imagenes> imagesList = [
+        Imagenes(
+          nombre: images.nombre,
+          mimeType: images.mimeType,
+          data: images.data,
+        ),
+      ];
 
-    List<Tramos> tramos = [
-      Tramos(
-        nroTramo: 0,
-        existingTo: 0,
-        aliasTo: '',
-        streetTo: origin.street,
-        cpTo: origin.postalCode,
-        cityTo: origin.city,
-        numberTo: origin.number,
-        countryTo: origin.country,
-        florNummberTo: '',
-        stateTo: origin.state,
-        descriptionTo: details.description,
-        placeIdTo: '',
-        latTo: origin.latitude,
-        longTo: origin.longitude,
-        longDirectionTo: '',
-        nroRemito: '',
-        tamanioCarga: 1,
-        pesoCarga: 1,
-        acotacionesCarga: '',
-        imagenes: imagesList,
-      ),
-      Tramos(
-        nroTramo: 1,
-        existingTo: 0,
-        aliasTo: '',
-        streetTo: destination!.street,
-        cpTo: destination.postalCode,
-        cityTo: destination.city,
-        numberTo: destination.number,
-        countryTo: destination.country,
-        florNummberTo: '',
-        stateTo: destination.state,
-        descriptionTo: details.description,
-        placeIdTo: '',
-        latTo: destination.latitude,
-        longTo: destination.longitude,
-        longDirectionTo: '',
-        nroRemito: '',
-        tamanioCarga: 1,
-        pesoCarga: 1,
-        acotacionesCarga: '',
-        imagenes: imagesList,
-      ),
-    ];
-    await ref
-        .read(createRequestStateNotifierProvider.notifier)
-        .createRequest(
-          1,
-          '',
-          origin.street,
-          origin.postalCode,
-          origin.city,
-          origin.number,
-          origin.country,
-          '',
-          origin.state,
-          '',
-          '',
-          origin.latitude,
-          origin.longitude,
-          '',
-          details.description,
-          details.startDate,
-          details.startTime,
-          '',
-          2,
-          2,
-          tramos,
-          payment,
-          distance?.distance.toInt(),
-          0,
-          0,
-          0,
-        );
+      List<Tramos> tramos = [
+        Tramos(
+          nroTramo: 0,
+          existingTo: 0,
+          aliasTo: '',
+          streetTo: origin.street,
+          cpTo: origin.postalCode,
+          cityTo: origin.city,
+          numberTo: origin.number,
+          countryTo: origin.country,
+          florNummberTo: '',
+          stateTo: origin.state,
+          descriptionTo: details.description,
+          placeIdTo: '',
+          latTo: origin.latitude,
+          longTo: origin.longitude,
+          longDirectionTo: '',
+          nroRemito: '',
+          tamanioCarga: 1,
+          pesoCarga: 1,
+          acotacionesCarga: '',
+          imagenes: imagesList,
+        ),
+        Tramos(
+          nroTramo: 1,
+          existingTo: 0,
+          aliasTo: '',
+          streetTo: destination!.street,
+          cpTo: destination.postalCode,
+          cityTo: destination.city,
+          numberTo: destination.number,
+          countryTo: destination.country,
+          florNummberTo: '',
+          stateTo: destination.state,
+          descriptionTo: details.description,
+          placeIdTo: '',
+          latTo: destination.latitude,
+          longTo: destination.longitude,
+          longDirectionTo: '',
+          nroRemito: '',
+          tamanioCarga: 1,
+          pesoCarga: 1,
+          acotacionesCarga: '',
+          imagenes: imagesList,
+        ),
+      ];
+
+      await ref
+          .read(createRequestStateNotifierProvider.notifier)
+          .createRequest(
+            1,
+            '',
+            origin.street,
+            origin.postalCode,
+            origin.city,
+            origin.number,
+            origin.country,
+            '',
+            origin.state,
+            '',
+            '',
+            origin.latitude,
+            origin.longitude,
+            '',
+            details.description,
+            details.startDate,
+            details.startTime,
+            '',
+            2,
+            2,
+            tramos,
+            payment,
+            distance?.distance.toInt(),
+            0,
+            0,
+            0,
+          );
+
+      await controller.runJavaScript("""
+        document.getElementById("message").textContent = "✅ Solicitud creada con éxito";
+        setTimeout(() => window.location.href = "success://home", 1000);
+      """);
+      return true;
+    } catch (e) {
+      await controller.runJavaScript("""
+      document.getElementById("message").textContent = "❌ Error al crear la solicitud.";
+    """);
+      return false;
+    } finally {
+      await controller.runJavaScript("""
+      document.getElementById("pay-button").classList.remove("hidden");
+      document.getElementById("spinner").classList.add("hidden");
+    """);
+    }
   }
 
   @override
