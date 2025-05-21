e57cbed355f275694a5671f8ecc885f04a828d23
ivan kopech <ivankopech@gmail.com>
2025-05-21 12:39:48 -0300

    change filename

--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppAssets {
-  static const String loginJheet = 'assets/images/loginjeet.png';
+  static const String loginJeet = 'assets/images/loginjeet.png';
   // static const String logoSvg = 'assets/icons/logo.svg';
   // static const String otroPng = 'assets/images/otro.png';
 }
