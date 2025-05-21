05b0ff4307682b40c10283d2d3a392bfb885a0c0
ivan kopech <ivankopech@gmail.com>
2025-05-21 12:37:28 -0300

    auth screen widget

---
+++ lib/features/auth/presentation/widgets/login_widget.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:go_router/go_router.dart';
+import '../../../../common/widgets/loader_widget.dart';
+import '../../../../config/api_exception.dart';
+import '../../data/models/auth_response_model.dart';
+import '../providers/login_state_notifier_provider.dart';
+import '../../../home/presentation/screens/home_screen.dart';
+import '../../../../utils/utils.dart';
+import '../../../verification_code/providers/send_code_state_notifier_provider.dart';
+
+class LoginWidget extends ConsumerStatefulWidget {
+  const LoginWidget({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends ConsumerState<LoginWidget> {
+  final formKey = GlobalKey<FormBuilderState>();
+  TextEditingController numberController = TextEditingController();
+  TextEditingController codeController = TextEditingController();
+  bool isVerifying = false;
+  bool showCodeField = false;
+  String number = '';
+  String code = '';
+
+  Future<void> handleRequest() async {
+    setState(() {
+      isVerifying = true;
+      number = numberController.text.trim();
+    });
+    if (number.isEmpty) return;
+
+    try {
+      await ref.read(sendCodeStateNotifierProvider.notifier).sendCode(number);
+      setState(() => showCodeField = true);
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('An error ocurred')));
+    } finally {
+      setState(() => isVerifying = false);
+    }
+  }
+
+  Future<void> handleLogin() async {
+    setState(() {
+      code = codeController.text.trim();
+    });
+    if (code.isEmpty) return;
+
+    try {
+      await ref.read(loginStateNotifierProvider.notifier).login(number, code);
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('An error ocurred')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ref.listen<AsyncValue<AuthResponseModel?>>(loginStateNotifierProvider, (
+      previous,
+      next,
+    ) {
+      if (next is AsyncData && next.value != null) {
+        context.go(HomeScreen.path);
+      }
+      if (next is AsyncError) {}
+    });
+    final authState = ref.watch(loginStateNotifierProvider);
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: appColors.backgroundColor,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: appPaddings.medium,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Welcome!',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    'Inicia sesión para continuar',
+                    style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                  ),
+                  const SizedBox(height: 24),
+                  SizedBox(width: 200, child: appImage(AppAssets.loginJeet)),
+                  const SizedBox(height: 24),
+
+                  // FORM SECTION
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      //border: Border.all(color: appColors.primaryColor),
+                    ),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: numberController,
+                            keyboardType: TextInputType.phone,
+                            decoration: const InputDecoration(
+                              hintText: 'Enter your phone number',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20),
+                                ),
+                              ),
+                              prefixIcon: Icon(Icons.phone),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          if (showCodeField) ...[
+                            TextField(
+                              controller: codeController,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter verification code',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(20),
+                                  ),
+                                ),
+                                prefixIcon: Icon(Icons.lock),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                          ],
+                          SizedBox(
+                            width: double.infinity,
+                            child:
+                                isVerifying
+                                    ? const Center(
+                                      child: CircularProgressIndicator(),
+                                    )
+                                    : ElevatedButton(
+                                      onPressed:
+                                          showCodeField
+                                              ? handleLogin
+                                              : handleRequest,
+                                      style: ElevatedButton.styleFrom(
+                                        padding: const EdgeInsets.symmetric(
+                                          vertical: 14,
+                                        ),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                            12,
+                                          ),
+                                        ),
+                                      ),
+                                      child: Text(
+                                        showCodeField ? 'Log in' : 'Send code',
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                    ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  // Container(
+                  //   padding: const EdgeInsets.all(20),
+                  //   decoration: BoxDecoration(
+                  //     color: const Color(0xFFFDF3FA),
+                  //     borderRadius: BorderRadius.circular(20),
+                  //     border: Border.all(
+                  //       color: appColors.primaryColor.withOpacity(0.3),
+                  //     ),
+                  //   ),
+                  //   child: FormBuilder(
+                  //     key: formKey,
+                  //     child: Column(
+                  //       children: <Widget>[
+                  //         FormBuilderTextField(
+                  //           name: 'email',
+                  //           decoration: AppInputStyles.inputDecoration(
+                  //             label: 'Email',
+                  //             hintText: 'ejemplo@correo.com',
+                  //             prefixIcon: const Icon(Icons.email),
+                  //           ),
+                  //           textInputAction: TextInputAction.next,
+                  //           validator: FormBuilderValidators.compose([
+                  //             FormBuilderValidators.required(),
+                  //             // FormBuilderValidators.email(),
+                  //           ]),
+                  //         ),
+                  //         const SizedBox(height: 16),
+                  //         FormBuilderTextField(
+                  //           name: 'password',
+                  //           obscureText: true,
+                  //           decoration: AppInputStyles.inputDecoration(
+                  //             label: 'Password',
+                  //             prefixIcon: const Icon(Icons.lock),
+                  //           ),
+                  //           validator: FormBuilderValidators.required(),
+                  //         ),
+                  //         const SizedBox(height: 24),
+                  //         authState is AsyncLoading
+                  //             ? const LoaderWidget()
+                  //             : SizedBox(
+                  //               width: double.infinity,
+                  //               child: ElevatedButton(
+                  //                 style: AppButtonStyles.primaryButton,
+                  //                 onPressed: () async {
+                  //                   await handleLogin();
+                  //                   // if (formKey.currentState!
+                  //                   //     .saveAndValidate()) {
+                  //                   //   final formData =
+                  //                   //       formKey.currentState!.value;
+                  //                   //   final email = formData['email'];
+                  //                   //   final password = formData['password'];
+                  //                   //   await ref
+                  //                   //       .read(
+                  //                   //         loginStateNotifierProvider.notifier,
+                  //                   //       )
+                  //                   //       .login(email, password);
+                  //                   // }
+                  //                 },
+                  //                 child: const Text("Login"),
+                  //               ),
+                  //             ),
+                  //       ],
+                  //     ),
+                  //   ),
+                  // ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
