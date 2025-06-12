292ce29a572e29a2c6febc460409e8c2e9ec3b00
ivan kopech <ivankopech@gmail.com>
2025-06-12 15:30:20 -0300

    delete commented code

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -18,35 +18,5 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return HomeContent();
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     leading: Builder(
-    //       builder:
-    //           (context) => IconButton(
-    //             icon: const Icon(Icons.menu, color: Colors.white),
-    //             onPressed: () => Scaffold.of(context).openDrawer(),
-    //           ),
-    //     ),
-    //     backgroundColor: appColors.primaryColor,
-    //   ),
-    //   drawer: AppDrawer(),
-    //   body: Column(
-    //     children: [
-    //       Container(
-    //         margin: const EdgeInsets.only(top: 250),
-    //         child: Text(
-    //           'Welcome back!',
-    //           style: TextStyle(fontSize: 35, fontWeight: FontWeight.w400),
-    //         ),
-    //       ),
-    //       Expanded(
-    //         child: Align(
-    //           alignment: Alignment.bottomCenter,
-    //           child: Image.asset('assets/images/loginjeet.png'),
-    //         ),
-    //       ),
-    //     ],
-    //   ),
-    // );
   }
 }
