0e46ab7a2accdd55b91f792cc836c6f66c89c910
ivan kopech <ivankopech@gmail.com>
2025-06-12 15:29:40 -0300

    definitive home screen

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -3,11 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../auth/presentation/providers/login_state_notifier_provider.dart';
 import '../providers/user_state_notifier_provider.dart';
 import '../widgets/home_content.dart';
-import '../../../map/presentation/screens/map_input.dart';
-import '../../../requests/presentation/screens/get_requests_screen.dart';
-import '../../../../utils/utils.dart';
-
-import '../widgets/app_drawer.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -20,36 +15,38 @@ class HomeScreen extends ConsumerStatefulWidget {
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final userId =
-          ref
-              .read(loginStateNotifierProvider.notifier)
-              .authResponseModel
-              ?.result
-              ?.userId;
-      if (userId != null) {
-        ref.read(userStateNotifierProvider.notifier).getUserData(userId);
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Builder(
-          builder:
-              (context) => IconButton(
-                icon: const Icon(Icons.menu, color: Colors.white),
-                onPressed: () => Scaffold.of(context).openDrawer(),
-              ),
-        ),
-        backgroundColor: appColors.primaryColor,
-      ),
-      drawer: AppDrawer(),
-      body: Center(child: Text('data')),
-    );
+    return HomeContent();
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     leading: Builder(
+    //       builder:
+    //           (context) => IconButton(
+    //             icon: const Icon(Icons.menu, color: Colors.white),
+    //             onPressed: () => Scaffold.of(context).openDrawer(),
+    //           ),
+    //     ),
+    //     backgroundColor: appColors.primaryColor,
+    //   ),
+    //   drawer: AppDrawer(),
+    //   body: Column(
+    //     children: [
+    //       Container(
+    //         margin: const EdgeInsets.only(top: 250),
+    //         child: Text(
+    //           'Welcome back!',
+    //           style: TextStyle(fontSize: 35, fontWeight: FontWeight.w400),
+    //         ),
+    //       ),
+    //       Expanded(
+    //         child: Align(
+    //           alignment: Alignment.bottomCenter,
+    //           child: Image.asset('assets/images/loginjeet.png'),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
--- lib/features/home/presentation/widgets/home_content.dart
+++ lib/features/home/presentation/widgets/home_content.dart
@@ -1,49 +1,180 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:move/features/home/presentation/widgets/app_drawer.dart';
 import '../../../../common/widgets/loader_widget.dart';
+import '../../../../common/widgets/generic_error_screen.dart';
 import '../providers/user_state_notifier_provider.dart';
 import '../../../../utils/utils.dart';
+import '../../../auth/presentation/providers/login_state_notifier_provider.dart';
+import '../providers/create_profile_state_notifier_provider.dart';
+import 'inputs.dart';
 
-class HomeContent extends ConsumerWidget {
+class HomeContent extends ConsumerStatefulWidget {
   const HomeContent({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeContent> createState() => _HomeContentState();
+}
+
+class _HomeContentState extends ConsumerState<HomeContent> {
+  int? userId;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController surnameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      userId =
+          ref
+              .read(loginStateNotifierProvider.notifier)
+              .authResponseModel
+              ?.result
+              ?.userId;
+      if (userId != null) {
+        ref.read(userStateNotifierProvider.notifier).getUserData(userId!);
+      }
+    });
+  }
+
+  void onRetry() {
+    if (userId != null) {
+      ref.read(userStateNotifierProvider.notifier).getUserData(userId!);
+    }
+  }
+
+  Widget showInputDialog() {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      backgroundColor: const Color(0xFFF3EDF7),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Enter Details',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 24),
+
+            buildTextField(
+              controller: emailController,
+              hint: 'Email',
+              icon: Icon(Icons.email_outlined),
+            ),
+            const SizedBox(height: 16),
+
+            buildTextField(
+              controller: nameController,
+              hint: 'Name',
+              icon: Icon(Icons.person_outline_rounded),
+            ),
+            const SizedBox(height: 16),
+
+            buildTextField(
+              controller: surnameController,
+              hint: 'Surname',
+              icon: Icon(Icons.person_outline_rounded),
+            ),
+            const SizedBox(height: 32),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildButton('Cancel', () {
+                  Navigator.pop(context);
+                  emailController.clear();
+                  nameController.clear();
+                  surnameController.clear();
+                }),
+                const SizedBox(width: 16),
+                buildButton('Submit', () async {
+                  await ref
+                      .read(createProfileStateNotifierProvider.notifier)
+                      .createProfile(
+                        emailController.text,
+                        nameController.text,
+                        surnameController.text,
+                      );
+                  emailController.clear();
+                  nameController.clear();
+                  surnameController.clear();
+                  context.pop();
+                }),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userState = ref.watch(userStateNotifierProvider);
 
     return userState.when(
-      data: (data) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Card(
-              margin: const EdgeInsets.all(10),
-              child: Container(
-                margin: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text('Move', style: TextStyle(color: Colors.grey)),
-                    Text(
-                      'Welcome ${ref.read(userStateNotifierProvider.notifier).userResponseModel?.fullName ?? ''}',
-                      style: const TextStyle(fontSize: 24),
-                    ),
-                  ],
+      data: (user) {
+        final name =
+            ref
+                .read(userStateNotifierProvider.notifier)
+                .userResponseModel
+                ?.name ??
+            '';
+        final surname =
+            ref
+                .read(userStateNotifierProvider.notifier)
+                .userResponseModel
+                ?.surname ??
+            '';
+        return Scaffold(
+          appBar: AppBar(),
+          drawer: AppDrawer(),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 250),
+                child: Text(
+                  'Welcome back, $name!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.w400),
                 ),
               ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: appImage(AppAssets.loginJeet),
+              if (name == surname) ...[
+                TextButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => showInputDialog(),
+                    );
+                  },
+                  child: Text(
+                    'Press here to complete your personal information',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: appImage(AppAssets.loginJeet),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
-      error:
-          (error, stackTrace) =>
-              const Center(child: Text('Error cargando usuario')),
+      error: (error, _) => GenericErrorScreen(onRetry: onRetry),
       loading: () => const LoaderWidget(),
     );
   }
