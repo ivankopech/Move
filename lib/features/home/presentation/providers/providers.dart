f3dcf594c1c4a65253a7fcad5b66f053e6fc0a42
ivan kopech <ivankopech@gmail.com>
2025-06-12 15:30:40 -0300

    create profile provider

--- lib/features/home/presentation/providers/providers.dart
+++ lib/features/home/presentation/providers/providers.dart
@@ -3,6 +3,9 @@ import '../../../../config/api_client_provider.dart';
 import '../../data/repositories/user_repository_interface_implementation.dart';
 import '../../domain/repositories/user_repository_interface.dart';
 import '../../domain/use_cases/user_data_use_case.dart';
+import '../../data/repositories/create_profile_repository_interface_implementation.dart';
+import '../../domain/repositories/create_profile_repository_interface.dart';
+import '../../domain/use_cases/create_profile_use_case.dart';
 
 final userRepositoryInterfaceProvider = Provider<UserRepositoryInterface>((
   ref,
@@ -14,5 +17,20 @@ final userRepositoryInterfaceProvider = Provider<UserRepositoryInterface>((
 final userDataUseCaseProvider = Provider<UserDataUseCase>(
   (ref) => UserDataUseCase(
     userRepositoryInterface: ref.watch(userRepositoryInterfaceProvider),
+  ),
+);
+
+//CREATE PROFILE
+final createProfileRepositoryInterfaceProvider =
+    Provider<CreateProfileRepositoryInterface>((ref) {
+      final apiClient = ref.read(apiClientProvider);
+      return CreateProfileRepositoryInterfaceImplementation(apiClient);
+    });
+// USE CASE PROVIDERS
+final createProfileUseCaseProvider = Provider<CreateProfileUseCase>(
+  (ref) => CreateProfileUseCase(
+    createProfileRepositoryInterface: ref.watch(
+      createProfileRepositoryInterfaceProvider,
+    ),
   ),
 );
