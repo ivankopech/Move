78a7928af79778a57e5db000647c52bec02f3444
ivan kopech <ivankopech@gmail.com>
2025-06-17 13:58:18 -0300

    remove firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:move/config/router/router_provider.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
-  await Firebase.initializeApp();
   runApp(ProviderScope(child: const MyApp()));
 }
 
