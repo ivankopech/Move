f76feda9979fb54129ba08d47a07a3d251f5f22e
ivan kopech <ivankopech@gmail.com>
2025-06-17 14:02:47 -0300

    remove reference to deleted screens

--- lib/features/map/presentation/screens/screens.dart
+++ lib/features/map/presentation/screens/screens.dart
@@ -2,6 +2,4 @@ export 'address_input.dart';
 export 'map_input.dart';
 export 'arrival_time.dart';
 export 'item_information.dart';
-export 'phone_number.dart';
-export 'code_verification.dart';
 export 'payment_input.dart';
