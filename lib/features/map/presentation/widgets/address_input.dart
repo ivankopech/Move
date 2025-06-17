73dcb1c30075a6cac661b7ccc14e86c4dc741eee
ivan kopech <ivankopech@gmail.com>
2025-06-17 14:03:14 -0300

    remove unused import

--- lib/features/map/presentation/widgets/address_input.dart
+++ lib/features/map/presentation/widgets/address_input.dart
@@ -7,7 +7,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
-import 'dart:math' show asin, atan2, cos, pi, sin, sqrt;
 
 import 'package:move/features/map/data/models/request_info.dart';
 import 'package:move/features/map/presentation/providers/providers.dart';
