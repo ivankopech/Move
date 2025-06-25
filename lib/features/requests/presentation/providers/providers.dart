6c9ddbb4b4cb6ee3675c33c64deb50e69f800cb4
ivan kopech <ivankopech@gmail.com>
2025-06-25 14:56:22 -0300

    track request implementation

--- lib/features/requests/presentation/providers/providers.dart
+++ lib/features/requests/presentation/providers/providers.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../config/api_client_provider.dart';
 import '../../data/repositories/get_request_repository_interface_implementation.dart';
+import '../../data/repositories/track_request_repository_interface_implementation.dart';
 import '../../domain/repositories/get_requests_repository_interface.dart';
+import '../../domain/repositories/track_request_repository_interface.dart';
 import '../../domain/use_cases/get_requests_data_use_case.dart';
+import '../../domain/use_cases/track_request_data_use_case.dart';
 
 final getRequestsRepositoryInterfaceProvider =
     Provider<GetRequestsRepositoryInterface>((ref) {
@@ -15,6 +18,21 @@ final getRequestsDataUseCaseProvider = Provider<GetRequestsDataUseCase>(
   (ref) => GetRequestsDataUseCase(
     getRequestsRepositoryInterface: ref.watch(
       getRequestsRepositoryInterfaceProvider,
+    ),
+  ),
+);
+
+final trackRequestRepositoryInterfaceProvider =
+    Provider<TrackRequestRepositoryInterface>((ref) {
+      final apiClient = ref.read(apiClientProvider);
+      return TrackRequestRepositoryInterfaceImplementation(apiClient);
+    });
+
+//USE CASE PROVIDER
+final trackRequestDataUseCaseProvider = Provider<TrackRequestDataUseCase>(
+  (ref) => TrackRequestDataUseCase(
+    trackRequestRepositoryInterface: ref.watch(
+      trackRequestRepositoryInterfaceProvider,
     ),
   ),
 );
