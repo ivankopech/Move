6a01a63e62d3acf31c9180bc262c830a59255211
ivan kopech <ivankopech@gmail.com>
2025-07-07 16:34:40 -0300

    modify imports

--- lib/features/requests/presentation/providers/track_request_state_notifier_provider.dart
+++ lib/features/requests/presentation/providers/track_request_state_notifier_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../data/models/track_request_model.dart';
-import '../../presentation/providers/providers.dart';
-import 'state/track_request_state_notifier.dart';
+import '../providers/providers.dart';
+import '../providers/state/track_request_state_notifier.dart';
 
 final trackRequestStateNotifierProvider = StateNotifierProvider<
   TrackRequestStateNotifier,
