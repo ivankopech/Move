c5f1144a9afe5ed694fa5a0f0af79c6ff337741e
ivan kopech <ivankopech@gmail.com>
2025-07-07 16:33:37 -0300

    endpoint modified

--- lib/features/requests/data/repositories/track_request_repository_interface_implementation.dart
+++ lib/features/requests/data/repositories/track_request_repository_interface_implementation.dart
@@ -6,19 +6,19 @@ import '../../domain/repositories/track_request_repository_interface.dart';
 
 class TrackRequestRepositoryInterfaceImplementation
     extends TrackRequestRepositoryInterface {
-  final ApiClient apiClient;
+  final ApiClient _apiClient;
 
-  TrackRequestRepositoryInterfaceImplementation(this.apiClient);
+  TrackRequestRepositoryInterfaceImplementation(this._apiClient);
 
   @override
   Future<Result<TrackRequestModel?>> trackRequest(int id) async {
     try {
-      final response = await apiClient.getData(
-        'services/app/Solicitud/GetTracking?serviceId=$id&skipCount=0&maxResultCount=1',
+      final response = await _apiClient.getData(
+        'services/app/Solicitud/GetTracking/$id?skipCount=0&maxResultCount=1',
         (json) => TrackRequestModel.fromJson(json),
       );
       return response.fold((error) => Left(error), (data) async {
-        return Right(data!);
+        return Right(data);
       });
     } catch (e, stackTrace) {
       return Left(
