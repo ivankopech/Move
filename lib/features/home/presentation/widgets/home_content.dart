9c816a59d39db66416722ab90470fbb3f8c1c73a
ivan kopech <ivankopech@gmail.com>
2025-07-08 15:05:40 -0300

    display accepted requests

--- lib/features/home/presentation/widgets/home_content.dart
+++ lib/features/home/presentation/widgets/home_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 import 'package:move/features/home/presentation/widgets/app_drawer.dart';
 import '../../../../common/widgets/loader_widget.dart';
 import '../../../../common/widgets/generic_error_screen.dart';
@@ -9,6 +10,8 @@ import '../../../../utils/utils.dart';
 import '../../../auth/presentation/providers/login_state_notifier_provider.dart';
 import '../providers/create_profile_state_notifier_provider.dart';
 import 'inputs.dart';
+import '../../../requests/presentation/providers/get_requests_state_notifier_provider.dart';
+import '../../../requests/presentation/screens/get_requests_screen.dart';
 
 class HomeContent extends ConsumerStatefulWidget {
   const HomeContent({super.key});
@@ -36,6 +39,11 @@ class _HomeContentState extends ConsumerState<HomeContent> {
       if (userId != null) {
         ref.read(userStateNotifierProvider.notifier).getUserData(userId!);
       }
+    });
+    Future.microtask(() {
+      ref
+          .read(getRequestsStateNotifierProvider.notifier)
+          .getRequests('Accepted');
     });
   }
 
@@ -120,6 +128,8 @@ class _HomeContentState extends ConsumerState<HomeContent> {
   @override
   Widget build(BuildContext context) {
     final userState = ref.watch(userStateNotifierProvider);
+    final requestState = ref.watch(getRequestsStateNotifierProvider);
+    final requests = requestState.value ?? [];
 
     return userState.when(
       data: (user) {
@@ -141,14 +151,89 @@ class _HomeContentState extends ConsumerState<HomeContent> {
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                margin: const EdgeInsets.only(top: 250),
-                child: Text(
-                  'Welcome back, $name!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.w400),
-                ),
+              const SizedBox(height: 20),
+              Text(
+                'Welcome back, $name!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w300),
               ),
+
+              SizedBox(height: 20),
+              if (requests.isNotEmpty) ...[
+                GestureDetector(
+                  onTap: () => context.pushNamed(GetRequestsScreen.name),
+                  child: SizedBox(
+                    height: 500,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: requests.length,
+                      itemBuilder: (context, index) {
+                        final request = requests[index];
+                        final date = request!.fechaViaje;
+                        final id = request.id;
+                        DateTime dateTime = DateTime.parse(date.toString());
+                        String requestDate = DateFormat(
+                          'dd/MM/yyyy',
+                        ).format(dateTime);
+                        return Card(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 5,
+                          ),
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'ID: $id',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      requestDate,
+                                      style: TextStyle(color: Colors.grey[600]),
+                                    ),
+                                  ],
+                                ),
+                                //SizedBox(height: 8),
+                                Text(
+                                  'Origin: ${request.calleDesde} ${request.numeroDesde}',
+                                ),
+                                Text(
+                                  'Destination: ${request.calleHasta} ${request.numeroHasta}',
+                                ),
+                                SizedBox(height: 8),
+
+                                // Status
+                                Text(
+                                  'Status: ${request.estado}',
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ] else ...[
+                const Center(
+                  child: Text('You currently have no accepted requests'),
+                ),
+              ],
+
+              SizedBox(height: 20),
               if (name == surname) ...[
                 TextButton(
                   onPressed: () {
