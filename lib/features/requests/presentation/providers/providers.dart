0540fcac3be6a8dfd49e993b9a2216594945379f
ivan kopech <ivankopech@gmail.com>
2025-08-01 16:25:58 -0300

    set tip provider

--- lib/features/requests/presentation/providers/providers.dart
+++ lib/features/requests/presentation/providers/providers.dart
@@ -1,7 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:move/features/requests/domain/repositories/set_tip_repository_interface.dart';
+import 'package:move/features/requests/domain/use_cases/set_tip_data_use_case.dart';
 import '../../../../config/api_client_provider.dart';
 import '../../data/repositories/get_request_repository_interface_implementation.dart';
 import '../../data/repositories/track_request_repository_interface_implementation.dart';
+import '../../data/repositories/set_tip_repository_interface_implementation.dart';
 import '../../domain/repositories/get_requests_repository_interface.dart';
 import '../../domain/repositories/track_request_repository_interface.dart';
 import '../../domain/use_cases/get_requests_data_use_case.dart';
@@ -34,5 +37,19 @@ final trackRequestDataUseCaseProvider = Provider<TrackRequestDataUseCase>(
     trackRequestRepositoryInterface: ref.watch(
       trackRequestRepositoryInterfaceProvider,
     ),
+  ),
+);
+
+//SET TIP
+final setTipRepositoryInterfaceProvider = Provider<SetTipRepositoryInterface>((
+  ref,
+) {
+  final apiClient = ref.read(apiClientProvider);
+  return SetTipRepositoryInterfaceImplementation(apiClient);
+});
+// USE CASE PROVIDERS
+final setTipUseCaseProvider = Provider<SetTipUseCase>(
+  (ref) => SetTipUseCase(
+    setTipRepositoryInterface: ref.watch(setTipRepositoryInterfaceProvider),
   ),
 );
