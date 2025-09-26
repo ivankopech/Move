5f542569d140c909ad8d9d40bc225f8d54fd42e8
ivan kopech <ivankopech@gmail.com>
2025-09-26 16:28:23 -0300

    firebase services config

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,6 +2,8 @@ import Flutter
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import FirebaseCore
+import FirebaseMessaging
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,6 +13,7 @@ import GooglePlaces
   ) -> Bool {
     GMSServices.provideAPIKey(Secrets.apiKey)
     GMSPlacesClient.provideAPIKey(Secrets.apiKey)
+    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
