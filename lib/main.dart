81550ce77cba12f9e13af0a1dacb4bc800960600
ivan kopech <ivankopech@gmail.com>
2025-09-26 16:20:25 -0300

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -6,6 +7,8 @@ import 'package:move/config/router/router_provider.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ProviderScope(child: const MyApp()));
 
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
