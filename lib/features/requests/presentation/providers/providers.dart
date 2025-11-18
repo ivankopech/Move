58f09e8bc4042adbd76381106fc76096a33b9e19
ivan kopech <ivankopech@gmail.com>
2025-11-18 15:58:38 -0300

    track delivery feature

--- lib/features/requests/presentation/providers/providers.dart
+++ lib/features/requests/presentation/providers/providers.dart
@@ -3,12 +3,12 @@ import 'package:move/features/requests/domain/repositories/set_tip_repository_in
 import 'package:move/features/requests/domain/use_cases/set_tip_data_use_case.dart';
 import '../../../../config/api_client_provider.dart';
 import '../../data/repositories/get_request_repository_interface_implementation.dart';
-import '../../data/repositories/track_request_repository_interface_implementation.dart';
 import '../../data/repositories/set_tip_repository_interface_implementation.dart';
 import '../../domain/repositories/get_requests_repository_interface.dart';
-import '../../domain/repositories/track_request_repository_interface.dart';
 import '../../domain/use_cases/get_requests_data_use_case.dart';
-import '../../domain/use_cases/track_request_data_use_case.dart';
+import '../../data/repositories/get_tracking_repository_interface_implementation.dart';
+import '../../domain/repositories/get_tracking_repository_interface.dart';
+import '../../domain/use_cases/get_tracking_data_use_case.dart';
 
 final getRequestsRepositoryInterfaceProvider =
     Provider<GetRequestsRepositoryInterface>((ref) {
@@ -25,21 +25,6 @@ final getRequestsDataUseCaseProvider = Provider<GetRequestsDataUseCase>(
   ),
 );
 
-//TRACK REQUEST
-final trackRequestRepositoryInterfaceProvider =
-    Provider<TrackRequestRepositoryInterface>((ref) {
-      final apiClient = ref.read(apiClientProvider);
-      return TrackRequestRepositoryInterfaceImplementation(apiClient);
-    });
-// USE CASE PROVIDERS
-final trackRequestDataUseCaseProvider = Provider<TrackRequestDataUseCase>(
-  (ref) => TrackRequestDataUseCase(
-    trackRequestRepositoryInterface: ref.watch(
-      trackRequestRepositoryInterfaceProvider,
-    ),
-  ),
-);
-
 //SET TIP
 final setTipRepositoryInterfaceProvider = Provider<SetTipRepositoryInterface>((
   ref,
@@ -51,5 +36,21 @@ final setTipRepositoryInterfaceProvider = Provider<SetTipRepositoryInterface>((
 final setTipUseCaseProvider = Provider<SetTipUseCase>(
   (ref) => SetTipUseCase(
     setTipRepositoryInterface: ref.watch(setTipRepositoryInterfaceProvider),
+  ),
+);
+
+//GET TRACKING
+final getTrackingRepositoryInterfaceProvider =
+    Provider<GetTrackingRepositoryInterface>((ref) {
+      final apiClient = ref.read(apiClientProvider);
+      return GetTrackingRepositoryInterfaceImplementation(apiClient);
+    });
+
+//USE CASE PROVIDER
+final getTrackingDataUseCaseProvider = Provider<GetTrackingDataUseCase>(
+  (ref) => GetTrackingDataUseCase(
+    getTrackingRepositoryInterface: ref.watch(
+      getTrackingRepositoryInterfaceProvider,
+    ),
   ),
 );
